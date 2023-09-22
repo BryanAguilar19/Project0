065e22ed8f839a2b1137153a8d91abdd6051a7b6
Joshua M Furer <furerjos@shu.edu>
2023-09-22 14:45:17 -0400

    First company/car add

--- src/main/resources/Tables.sql
+++ src/main/resources/Tables.sql
@@ -8,4 +8,11 @@ create table Cars (car_id int primary key, car_name varchar(50), price float,
 
 create table Company (company_id primary, company_name varchar(50), country_name varchar(50));
 
+---------- Data Points -- Company ----------
+insert into Company (company_id, company_name, country_name) values (1, 'Ford', 'USA' );
+
 ---------- Data Points -- Cars ----------
+insert into Cars (car_id, car_name, price, mpg, company_id, company_id) values (1, Ford Bronco Sport, 37000, 25, 1);
+
+
+
