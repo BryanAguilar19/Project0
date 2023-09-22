80c8380f4407c1b338fb23a20e9a7e43c42515c4
Joshua M Furer <furerjos@shu.edu>
2023-09-22 14:33:15 -0400

    sql comment format

--- src/main/resources/Tables.sql
+++ src/main/resources/Tables.sql
@@ -1,10 +1,11 @@
--- dropping tables if they exist
+---------- dropping tables if they exist --------
 drop table Cars if exists;
 drop table Company if exists;
 
---Creating tables to insert informat
+---------- Creating tables to insert in format ----------
 create table Cars (car_id int primary key, car_name varchar(50), price float,
     mpg float ,company_id int, foreign key (company_id) references Company(company_id));
 
 create table Company (company_id primary, company_name varchar(50), country_name varchar(50));
 
+---------- Data Points -- Cars ----------
