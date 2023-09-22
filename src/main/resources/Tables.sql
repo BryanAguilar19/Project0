2b0c5a80e5302ab229c38ddf1a47304df9a37c42
Bryan Aguilar <aguilarbryan49@gmail.com>
2023-09-22 14:57:39 -0400

    Company Details

--- src/main/resources/Tables.sql
+++ src/main/resources/Tables.sql
@@ -10,6 +10,13 @@ create table Company (company_id primary, company_name varchar(50), country_name
 
 ---------- Data Points -- Company ----------
 insert into Company (company_id, company_name, country_name) values (1, 'Ford', 'USA' );
+insert into Company (company_id, company_name, country_name) values (2, 'Nissan', 'Japan' );
+insert into Company (company_id, company_name, country_name) values (3, 'Ferrari', 'Italy' );
+insert into Company (company_id, company_name, country_name) values (4, 'Toyota', 'Japan' );
+insert into Company (company_id, company_name, country_name) values (5, 'Mazda', 'Japan' );
+insert into Company (company_id, company_name, country_name) values (6, 'Kia', 'South Korea' );
+insert into Company (company_id, company_name, country_name) values (7, 'BMW', 'Germany' );
+insert into Company (company_id, company_name, country_name) values (8, 'Jeep', 'USA' );
 
 ---------- Data Points -- Cars ----------
 insert into Cars (car_id, car_name, price, mpg, company_id, company_id) values (1, Ford Bronco Sport, 37000, 25, 1);
